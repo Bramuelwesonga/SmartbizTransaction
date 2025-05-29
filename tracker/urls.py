9229db689ffb66690a9cb3502a49a5fb937cd0db
Bramuel wesonga <98466456+Bramuelwesonga@users.noreply.github.com>
2025-05-29 15:56:33 +0300

    Update urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('add/', views.add_transaction, name='add_transaction'),
     path('export-pdf/', views.export_pdf, name='export_pdf'),  # If you have export implemented
+    path('ajax/ocr/', views.ajax_receipt_ocr, name='ajax_receipt_ocr'),
 ]
